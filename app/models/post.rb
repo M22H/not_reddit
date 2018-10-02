e38dff15c3bc4ba699182441bfbcb88384caa955
Mitchell <mitchell.harris@outlook.com>
2018-10-02 17:39:49 +0100

    big commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+ has_many :comments, dependent: :destroy
+      # dependent: :destroy means the comments related
+      # to the specific post in mention get deleted if the post does.
+
+
 end
